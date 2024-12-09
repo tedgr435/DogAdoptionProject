cf9b1ccef664e157adafab7287334d111330fdd2
wanjiang xie <wanjiang.xie@gmail.com>
2024-12-09 14:20:35 -0600

    sq

---
+++ Data/DogAdoptionProjectContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DogAdoptionProject.Models;
+
+namespace DogAdoptionProject.Data
+{
+    public class DogAdoptionProjectContext : DbContext
+    {
+        public DogAdoptionProjectContext (DbContextOptions<DogAdoptionProjectContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DogAdoptionProject.Models.Application> Application { get; set; } = default!;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,15 @@
-using DogAdoptionProject.Components;
+﻿using DogAdoptionProject.Components;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using DogAdoptionProject.Data;
 
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddDbContextFactory<DogAdoptionProjectContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DogAdoptionProjectContext") ?? throw new InvalidOperationException("Connection string 'DogAdoptionProjectContext' not found.")));
+
+builder.Services.AddQuickGridEntityFrameworkAdapter();
+
+builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
@@ -14,6 +23,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 
 app.UseHttpsRedirection();
