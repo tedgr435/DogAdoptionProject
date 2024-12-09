98c402929d4b4c1c5da16cd405050ab333e7e945
wanjiang xie <wanjiang.xie@gmail.com>
2024-12-09 14:14:47 -0600

    Dog Update

--- Models/Dog.cs
+++ Models/Dog.cs
@@ -1,6 +1,25 @@
-﻿namespace DogAdoptionProject.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace DogAdoptionProject.Models
 {
     public class Dog
     {
+
+        public int Id { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string? Name { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string? Description { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string? Status { get; set; }
+        [Required]
+        [StringLength(60, MinimumLength = 3)]
+        public string? Type { get; set; }
+        //public string? pictureUrl { get; set; }
+        public Dog() { }
+
     }
 }
